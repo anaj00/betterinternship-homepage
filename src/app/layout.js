f73fcbc21b89f2fd8f7ccc27f89e4822a0b7013c
anaj00 <jana_bantolino@dlsu.edu.ph>
2025-07-31 00:03:25 +0800

    fix: update layout structure by removing footer and adjusting main container styles for better layout management fix: correct width calculation for JobCardCarousel container to ensure proper responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,50 +23,8 @@ export default function RootLayout({ children }) {
       <body>
         <Providers>
           <HeroNavbar />
-          <main className="pt-0 px-0 sm:px-6 sm:pt-4">{children}</main>
-          <footer className="w-full text-gray-700 px-6 py-12 bg-white border border-gray-300">
-            <div className="max-w-7xl mx-auto flex flex-col sm:flex-row justify-between gap-12">
-              {/* Brand Info */}
-              <div className="flex flex-col gap-4 max-w-md">
-                <div className="flex items-center gap-3">
-                  <img
-                    src="/logo-only.png"
-                    alt="BetterInternship Logo"
-                    className="w-10 h-10"
-                  />
-                  <span className="text-xl font-bold">BetterInternship</span>
-                </div>
-                <p className="text-sm text-gray-700 leading-relaxed">
-                  Your one-stop platform for discovering internships,
-                  streamlining your application process, and getting hired
-                  faster in the Philippines.
-                </p>
-              </div>
-
-              {/* Navigation Links */}
-              <div className="flex flex-col gap-4 text-sm">
-                <span className="font-semibold text-gray-700">Navigation</span>
-                <Link href="/" className="hover:underline">
-                  Home
-                </Link>
-                <Link href="/jobs" className="hover:underline">
-                  Job Board
-                </Link>
-                <Link href="/about" className="hover:underline">
-                  About Us
-                </Link>
-                <Link href="/contact" className="hover:underline">
-                  Contact
-                </Link>
-              </div>
-            </div>
-
-            {/* Bottom Note */}
-            <div className="mt-10 text-xs text-white/60 text-center">
-              © {new Date().getFullYear()} BetterInternship. All rights
-              reserved.
-            </div>
-          </footer>
+          <main className="t-0 px-0 sm:px-6 sm:pt-4 flex items-center justify-center min-h-screen overflow-x-hidden">{children}</main>
+          
         </Providers>
       </body>
     </html>
